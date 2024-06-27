1356829163feedbb0d46250c281a6fa04e60a01b
izder456 <izder456@disroot.org>
2024-06-27 12:38:49 -0500

    first commit

---
+++ src/crfetch.cr
@@ -0,0 +1,101 @@
+require "process"
+
+module Crfetch
+  VERSION = "0.1.0"
+
+  def self.runSysCommand(command : String) : String
+    # Implement running system command and yoinking output
+    output = IO::Memory.new
+    Process.run(command, shell: true, output: output)
+    output.close
+
+    output.to_s
+  end
+
+  def self.getPlatform : String?
+    # Implement getting system platform
+    case os = self.runSysCommand("uname").strip
+    when /Linux/
+      "Linux"
+    when /Darwin/
+      "macOS"
+    when /FreeBSD/
+      "FreeBSD"
+    when /OpenBSD/
+      "OpenBSD"
+    when /NetBSD/
+      "NetBSD"
+    else
+      "I dunno"
+    end
+  end
+
+  def self.getMemory : String?
+    # Implement Getting Memory Usage
+    os = getPlatform
+    memory_command = case os
+                     when "Linux"
+                       "cat /proc/meminfo"
+                     when "macOS"
+                       "sysctl -n hw.memsize"
+                     when /BSD/
+                       "sysctl -n hw.physmem"
+                     else
+                       ""
+                     end
+
+    if memory_command.empty?
+      puts "Unsupported OS: #{os}"
+    else
+      megabyte = 1048576
+      memory = self.runSysCommand(memory_command).strip.to_f / megabyte
+      output = "%.2f" % memory
+    end
+  end
+
+  def self.getMemoryUsage : String?
+    # Implement getting memory usage
+    os = getPlatform
+    case os
+    when "Linux"
+      command = "free -m"
+      self.runSysCommand(command).strip
+    when "macOS"
+      command = "vm_stat | grep 'Pages active' | awk '{print $3}' | sed 's/\.$//' | awk '{printf \"%.2f\\n\", $1*4096/1024/1024}'"
+      runSysCommand(command).strip
+    when /BSD/
+      command = "vmstat -s | grep 'pages active' | awk '{print $1}' | awk '{printf \"%.2f\\n\", $1*4096/1024/1024}'"
+      runSysCommand(command).strip
+    else
+      nil
+    end
+  end
+
+  def self.getCpu : String?
+    # Implement Getting CPU name
+    os = getPlatform
+    case os
+    when "Linux"
+      cpu_info = File.read("/proc/cpuinfo")
+      match = cpu_info.match(/modelname\s+:\s+(.+)/)
+      match[1] if match
+    when "macOS"
+      cpu_info = runSysCommand("sysctl -n machdep.cpu.brand_string").strip
+      cpu_info unless cpu_info.empty?
+    when /BSD/
+      cpu_info = runSysCommand("sysctl -n hw.model").strip
+      cpu_info unless cpu_info.empty?
+    else
+      nil
+    end
+  end
+end
+
+os = Crfetch.getPlatform
+mem_usage = Crfetch.getMemoryUsage
+mem = Crfetch.getMemory
+cpu = Crfetch.getCpu
+
+puts "OS:  #{os}"
+puts "MEM: #{mem_usage}/#{mem}MB"
+puts "CPU: #{cpu}"
